e986dfd530732eeaee459851b87f35db6cbdb658
rhaynes3k <rhaynes3k@gmail.com>
2021-06-02 17:10:40 -0500

    done

--- index.js
+++ index.js
@@ -7,9 +7,20 @@ function updateDriverWithKeyAndValue(obj, key, val){
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(obj, key, val) {
-  
+  obj[key] = val
+  return obj
 }
 
+function deleteFromDriverByKey(obj, key) {
+  let newDriver = {...obj}
+  delete newDriver[key]
+  return newDriver
+}
+
+function destructivelyDeleteFromDriverByKey(obj, key) {
+  delete obj[key]
+  return obj
+}
 //let updateDriverWithKeyAndValue = function(obj, key, val){
 //  return Object.assign({}, obj, {[key]: val})
 //}
